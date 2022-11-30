63a1dcd6abc775acad91acc9a8a964178339cd37
Rafael Anjos Santos <68437256+YoungC0DE@users.noreply.github.com>
2022-11-30 10:58:59 -0300

    fix

--- api/components/navbar.php
+++ api/components/navbar.php
@@ -25,10 +25,9 @@
             <i class="bi bi-grid-3x3-gap text-light fs-2 ms-2"></i>
         </a>
         <div class="d-flex flex-row gap-4 navbar-nav">
-            <a class="nav-link active" aria-current="page" href="?page=bhaskara">Bhaskara</a>
-            <a class="nav-link active" aria-current="page" href="?page=bhaskara">Equação 2º grau</a>
-            <a class="nav-link active" aria-current="page" href="?page=bhaskara">Fibonacci</a>
-            <a class="nav-link active" aria-current="page" href="?page=bhaskara">MMC</a>
+            <a class="nav-link active" aria-current="page" href="?page=Bhaskara">Bhaskara</a>
+            <a class="nav-link active" aria-current="page" href="?page=Fibonacci">Fibonacci</a>
+            <a class="nav-link active" aria-current="page" href="?page=MMC">MMC</a>
         </div>
         <a href="https://github.com/YoungC0DE" target="_blank">
             <i class="bi bi-github text-light fs-2 me-2"></i>
--- api/pages/Fibonacci.php
+++ api/pages/Fibonacci.php
@@ -1,32 +1,70 @@
-<div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1">@</span>
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+<style>
+  .form-control {
+    box-shadow: none !important;
+  }
+
+  #result {
+    min-height: 180px;
+    max-height: 180px;
+    overflow: auto;
+  }
+</style>
+
+<div class="text-center align-middle mb-5">
+  <span class="p-2 border rounded fs-2">Fn = fn-1 + Fn-2</span>
 </div>
 
-<div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-  <span class="input-group-text" id="basic-addon2">@example.com</span>
+<span class="mb-2 text-secondary">Preencha os valores para o calculo</span>
+<form class="d-flex flex-row w-50 gap-3 mb-3">
+  <div class="input-group has-validation">
+    <span class="input-group-text rounded-0 rounded-start">O valor</span>
+    <input type="number" min=0 value=0 class="form-control" id="limitN">
+    <span class="input-group-text rounded-0">É igual a</span>
+    <input type="number" placeholder="-" class="form-control rounded-0" id="valueF" readonly>
+    <span class="input-group-text rounded-0 rounded-end">Na sequência Fibonacci</span>
+  </div>
+</form>
+
+<div class="d-flex flex-row gap-3">
+  <button type="button" class="btn btn-secondary mb-3 rounded-1" onclick="clean()">Limpar</button>
+  <button type="button" class="btn btn-primary mb-3 rounded-1" onclick="calc()">Calcular</button>
+  <button type="button" class="btn btn-secondary mb-3 rounded-1" onclick="copy()">Copiar</button>
 </div>
 
-<label for="basic-url" class="form-label">Your vanity URL</label>
-<div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
-  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+<div class="form-floating w-50">
+  <textarea class="form-control text-center" id="result" readonly></textarea>
+  <label for="result">Resolução:</label>
 </div>
 
-<div class="input-group mb-3">
-  <span class="input-group-text">$</span>
-  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-  <span class="input-group-text">.00</span>
-</div>
+<script>
+  const result = document.getElementById('result')
 
-<div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username">
-  <span class="input-group-text">@</span>
-  <input type="text" class="form-control" placeholder="Server" aria-label="Server">
-</div>
+  const clean = () => {
+    result.textContent = ''
+  }
 
-<div class="input-group">
-  <span class="input-group-text">With textarea</span>
-  <textarea class="form-control" aria-label="With textarea"></textarea>
-</div>
+  const calc = () => {
+    const limitN = parseInt(document.getElementById('limitN').value)
+    let a1 = 1,
+    a2 = 1,
+    a3 = 0
+
+    clean()
+    result.textContent += `${a1} ➩ ${a2} ➩ `
+
+    for(let i = 3; i <= limitN; i++){
+      a3 = a1 + a2
+      result.textContent += `${a3} ➩ `
+      a2 = a1
+      a1 = a3
+    }
+    document.getElementById('valueF').value = a3
+  }
+
+  const copy = () => {
+    result.select()
+    result.setSelectionRange(0, 99999) /* For mobile devices */
+    navigator.clipboard.writeText(result.value)
+    alert('copiado')
+  }
+</script>
